708849f281713b0c1647891f4f814377fc5b80e1
tttung <631929041@qq.com>
2020-08-21 15:17:11 +0800

    on

--- Prognostics/featureEngineer.py
+++ Prognostics/featureEngineer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import math
 import pywt
 import numpy as np
--- Prognostics/preprocess.py
+++ Prognostics/preprocess.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import os
 import numpy as np
 from scipy.io import loadmat
